bb6a63485e37b783ec57382adff9ff995015f766
yesacs <casey.sullivan@gmail.com>
2015-10-08 21:13:22 -0400

    Line breaks

--- symlink.sh
+++ symlink.sh
@@ -2,6 +2,8 @@
 
 PWD=$(pwd)
 
+
+
 echo "Making vim symlinks for vim settings"
 rm ~/.vimrc; ln -s "$PWD"/vimrc.vimrc ~/.vimrc
 rm ~/.vim; ln -s "$PWD"/vim/ ~/.vim
@@ -12,12 +14,18 @@ rm ~/.vimrc.local;ln -s "$PWD"/vimrc.local ~/.vimrc.local
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+
+
 echo "Making symlinks for profile settings"
 rm ~/.profile; ln -sf "$PWD"/profile ~/.profile
 source ~/.profile
 
+
+
 echo "Making symlinks for git template"
 rm ~/.git_commit_message; ln -s "$PWD"/git_commit_message ~/.git_commit_message
 git config --global commit.template ~/.git_commit_message
+
+
 
 echo "All Done!"
