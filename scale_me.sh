9dff405f8b5dbbe23b3267af4ef27680a979e9e6
Casey Sullivan <casey.sullivan@gmail.com>
2019-03-25 22:53:34 -0400

    notes about scaling script

--- scale_me.sh
+++ scale_me.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 
-#https://techienotes.blog/2019/01/10/set-fractional-display-scaling-on-ubuntu-18-10-xorg/
+# https://techienotes.blog/2019/01/10/set-fractional-display-scaling-on-ubuntu-18-10-xorg/
+# this screws up sometimes, so move your xorg.conf and reboot
+# scale 1.4 works pretty well 
 
 gsettings set org.gnome.desktop.interface scaling-factor 2
 gsettings set org.gnome.settings-daemon.plugins.xsettings overrides "{'Gdk/WindowScalingFactor': <2>}" 
-xrandr --output eDP-1 --scale $1x$1
+xrandr --output eDP1 --scale $1x$1
