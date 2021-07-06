0b2e3544e758c719a41c80c4eacbe9060e938d1f
Casey Sullivan <casey.sullivan@gmail.com>
2021-07-06 15:34:46 -0400

    moar

--- fish/config.fish
+++ fish/config.fish
@@ -19,30 +19,16 @@ set yellow (set_color yellow)
 
 set -g __fish_git_prompt_showdirtystate 1
 set -g __fish_git_prompt_showuntrackedfiles 1
-set -g __fish_git_prompt_showupstream informative 
+set -g __fish_git_prompt_showstashstatus 1
+set -g __fish_git_prompt_showupstream 1
 set -g __fish_git_prompt_showcolorhints 1
-set -g __fish_git_prompt_use_informative_chars 0
-set -g __fish_git_prompt_show_informative_status 1
+# set -g __fish_git_prompt_show_informative_status 1
 
 set __fish_git_prompt_color_branch FF69B4 --bold
 # set __fish_git_prompt_color_dirtystate white
 # set __fish_git_prompt_color_invalidstate red
 # set __fish_git_prompt_color_merging yellow
 # set __fish_git_prompt_color_stagedstate yellow
-# set __fish_git_prompt_color_upstream_ahead green
-# set __fish_git_prompt_color_upstream_behind red
+set __fish_git_prompt_color_upstream_ahead yellow
+set __fish_git_prompt_color_upstream_behind red
 
-
-# Icons
-# set __fish_git_prompt_char_cleanstate '👍'
-# set __fish_git_prompt_char_conflictedstate '⚠️'
-# set __fish_git_prompt_char_dirtystate ' 💩'
-# set __fish_git_prompt_char_invalidstate ' 🤮'
-# set __fish_git_prompt_char_stagedstate ' 🚥'
-# set __fish_git_prompt_char_stashstate ' 📦'
-set __fish_git_prompt_char_stateseparator \UE0A0
-# set __fish_git_prompt_char_untrackedfiles ' 🔍'
-# set __fish_git_prompt_char_upstream_ahead '☝️'
-# set __fish_git_prompt_char_upstream_behind '👇'
-# set __fish_git_prompt_char_upstream_diverged ' 🚧'
-# set __fish_git_prompt_char_upstream_equal '💯'
