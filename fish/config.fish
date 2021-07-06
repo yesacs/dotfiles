f4beef322cdcf4344e33d9ba7eee3ab166b69c0f
Casey Sullivan <casey.sullivan@gmail.com>
2021-07-06 15:38:01 -0400

    what does verbose do

--- fish/config.fish
+++ fish/config.fish
@@ -20,7 +20,7 @@ set yellow (set_color yellow)
 set -g __fish_git_prompt_showdirtystate 1
 set -g __fish_git_prompt_showuntrackedfiles 1
 set -g __fish_git_prompt_showstashstatus 1
-set -g __fish_git_prompt_showupstream 1
+set -g __fish_git_prompt_showupstream verbose
 set -g __fish_git_prompt_showcolorhints 1
 # set -g __fish_git_prompt_show_informative_status 1
 
