a130b6ee5586b25fc0cd8278114ee4a2002a7a60
Casey Sullivan <casey.sullivan@gmail.com>
2021-07-06 14:24:12 -0400

    fish...

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,42 +1,46 @@
 function fish_prompt
-  set -l tomita_vi_mode "$TOMITA_VI"
+    set -l last_status $status
 
-  set_color -o yellow 
-  printf '%s:' (prompt_hostname)
-  set_color $fish_color_cwd
-  printf '%s' (prompt_pwd)
-  set_color normal
+    set -l normal (set_color normal)
+    set -l usercolor (set_color $fish_color_user)
 
-  if git_is_repo
-    set_color white
-    printf ' on '
+    set -l delim \U03BB
+    # If we don't have unicode use a simpler delimiter
+    string match -qi "*.utf-8" -- $LANG $LC_CTYPE $LC_ALL; or set delim ">"
 
-    set_color yellow
-    printf '%s' (git_branch_name)
-  end
+    fish_is_root_user; and set delim "#"
 
-  if test -z (string match -ri '^no|false|0$' $tomita_vi_mode)
-    printf ' '
-    switch $fish_bind_mode
-      case default
-        set_color -o green
-        printf '[N]'
-      case insert
-        set_color -o cyan
-        printf '[I]'
-      case visual
-        set_color -o red
-        printf '[V]'
-      case replace_one
-        set_color -o yellow
-        printf '[R]'
+    set -l cwd (set_color $fish_color_cwd)
+    if command -sq sha256sum
+        # randomized cwd color
+        set -l shas (pwd -P | sha256sum | string sub -l 6 | string match -ra ..)
+        # Increase color a bit so we don't get super dark colors.
+        # Really we want some contrast to the background (assuming black).
+        set -l col (for f in $shas; math --base=hex "min(255, 0x$f + 0x30)"; end | string replace 0x '' | string pad -c 0 -w 2 | string join "")
+
+        set cwd (set_color $col)
     end
-    set_color normal
-  end
-  
-  set_color -o white
-  # printf ' ⋊> '
-  printf ' λ '
-  set_color normal
 
+    # Prompt status only if it's not 0
+    set -l prompt_status
+    test $last_status -ne 0; and set prompt_status (set_color $fish_color_error)"[$last_status]$normal"
+
+    # Only show host if in SSH or container
+    # Store this in a global variable because it's slow and unchanging
+    if not set -q prompt_host
+        set -g prompt_host ""
+        if set -q SSH_TTY
+            or begin
+                command -sq systemd-detect-virt
+                and systemd-detect-virt -q
+            end
+            set -l host (hostname)
+            set prompt_host $usercolor$USER$normal@(set_color $fish_color_host)$host$normal":"
+        end
+    end
+
+    # Shorten pwd if prompt is too long
+    set -l pwd (prompt_pwd)
+
+    echo -n -s $prompt_host $cwd $pwd $normal $prompt_status ' ' $delim ' '
 end
