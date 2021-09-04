dc4ecf7f23a420d6a034afa4acd6a342fa854255
Casey Sullivan <casey.sullivan@gmail.com>
2021-09-04 07:43:52 -0400

    upgrade to new brew bash installer

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ if command -v brew
 then
  printf "😓 Homebrew installed already, skipping...\n\n"
 else
- /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 if command -v brew
