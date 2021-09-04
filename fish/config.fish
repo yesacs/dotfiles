97b6daaf61fd1a56b293a835dbf57ca7511d5068
Casey Sullivan <casey.sullivan@gmail.com>
2021-09-04 14:59:46 +0000

    more fzf in tmux changes

--- fish/config.fish
+++ fish/config.fish
@@ -3,8 +3,7 @@ set -x VISUAL nvim
 
 alias evil "env TERM=xterm-24bit emacs -nw" 
 alias nve "nvim -c 'CocCommand explorer'"
-alias fzf "fzf-tmux -p 80%,60%"
-alias gac "git add . && git commit"
+alias gg "git add . && git commit"
 
 contains /usr/local/sbin $fish_user_paths; or set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 contains /Users/casey/.emacs.d/bin $fish_user_paths; or set -g fish_user_paths "/Users/casey/.emacs.d/bin" $fish_user_paths
@@ -36,8 +35,10 @@ set __fish_git_prompt_char_stateseparator ':'
 set -x BAT_THEME OneHalfDark
 
 set -x FZF_DEFAULT_COMMAND  'rg --files --follow'
-set -x FZF_DEFAULT_OPTS '--preview "bat --color=always --style=numbers --line-range=:500 {}"'
+set -x FZF_DEFAULT_OPTS '--reverse --height 99% --preview "bat --color=always --style=numbers --line-range=:500 {}"'
 set -x FZF_CTRL_T_COMMAND '$FZF_DEFAULT_COMMAND'
 set -x FZF_CTRL_R_OPTS "--reverse --preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
+set -x FZF_TMUX 1
+set -x FZF_TMUX_OPTS '-p 80%,60%'
 
 fzf_configure_bindings --directory=\ct --git_status=\cs --git_log=\cg --history=\cr --variables=\cv
