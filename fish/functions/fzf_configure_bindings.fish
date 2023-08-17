f4b04d3e627921f0493f32766740ee6be7e1c43f
Casey Sullivan <casey.sullivan@gmail.com>
2023-08-17 09:48:30 -0400

    fisher and fzf fish updates and fixes

--- fish/functions/fzf_configure_bindings.fish
+++ fish/functions/fzf_configure_bindings.fish
@@ -4,10 +4,10 @@ function fzf_configure_bindings --description "Installs the default key bindings
     # no need to install bindings if not in interactive mode or running tests
     status is-interactive || test "$CI" = true; or return
 
-    set options_spec h/help 'directory=?' 'git_log=?' 'git_status=?' 'history=?' 'variables=?' 'git_branch=?' 
+    set -f options_spec h/help 'directory=?' 'git_log=?' 'git_status=?' 'history=?' 'processes=?' 'variables=?' 'git_branch=?'
     argparse --max-args=0 --ignore-unknown $options_spec -- $argv 2>/dev/null
     if test $status -ne 0
-        echo "Invalid option or a positional argument was provided." 1>&2
+        echo "Invalid option or a positional argument was provided." >&2
         _fzf_configure_bindings_help
         return 22
     else if set --query _flag_help
@@ -15,14 +15,15 @@ function fzf_configure_bindings --description "Installs the default key bindings
         return
     else
         # Initialize with default key sequences and then override or disable them based on flags
-        # index 1 = directory, 2 = git_log, 3 = git_status, 4 = history, 5 = variables
-        set key_sequences \e\cf \e\cl \e\cs \cr \cv \cq # \c = control, \e = escape
+        # index 1 = directory, 2 = git_log, 3 = git_status, 4 = history, 5 = processes, 6 = variables
+        set -f key_sequences \e\cf \e\cl \e\cs \cr \e\cp \cv \cq # \c = control, \e = escape
         set --query _flag_directory && set key_sequences[1] "$_flag_directory"
         set --query _flag_git_log && set key_sequences[2] "$_flag_git_log"
         set --query _flag_git_status && set key_sequences[3] "$_flag_git_status"
         set --query _flag_history && set key_sequences[4] "$_flag_history"
-        set --query _flag_variables && set key_sequences[5] "$_flag_variables"
-        set --query _flag_git_branch && set key_sequences[6] "$_flag_git_branch"
+        set --query _flag_processes && set key_sequences[5] "$_flag_processes"
+        set --query _flag_variables && set key_sequences[6] "$_flag_variables"
+        set --query _flag_git_branch && set key_sequences[7] "$_flag_git_branch"
 
         # If fzf bindings already exists, uninstall it first for a clean slate
         if functions --query _fzf_uninstall_bindings
@@ -34,8 +35,9 @@ function fzf_configure_bindings --description "Installs the default key bindings
             test -n $key_sequences[2] && bind --mode $mode $key_sequences[2] _fzf_search_git_log
             test -n $key_sequences[3] && bind --mode $mode $key_sequences[3] _fzf_search_git_status
             test -n $key_sequences[4] && bind --mode $mode $key_sequences[4] _fzf_search_history
-            test -n $key_sequences[5] && bind --mode $mode $key_sequences[5] "$_fzf_search_vars_command"
-            test -n $key_sequences[6] && bind --mode $mode $key_sequences[6] _fzf_search_git_branch
+            test -n $key_sequences[5] && bind --mode $mode $key_sequences[5] _fzf_search_processes
+            test -n $key_sequences[6] && bind --mode $mode $key_sequences[6] _fzf_search_vars_command
+            test -n $key_sequences[7] && bind --mode $mode $key_sequences[7] _fzf_search_git_branch
         end
 
         function _fzf_uninstall_bindings --inherit-variable key_sequences
