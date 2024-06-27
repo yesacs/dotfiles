58a06d0c1e82342543ff9824840a85220d2cd5e0
yesacs <casey.sullivan@gmail.com>
2024-06-27 10:58:50 -0400

    file name in status bar

--- astronvim/user.lua
+++ astronvim/user.lua
@@ -1,7 +1,13 @@
 return {
-	options = {
-		opt = {
-			showtabline = 0,
+	{
+		"AstroNvim/astrocore",
+		---@param opts AstroCoreOpts
+		opts = {
+			options = {
+				opt = {
+					showtabline = 0,
+				},
+			},
 		},
 	},
 	plugins = {
@@ -104,6 +110,33 @@ return {
 				"typescript",
 			},
 		},
+	},
+	{
+		"rebelot/heirline.nvim",
+		opts = function(_, opts)
+			local status = require("astroui.status")
+			opts.statusline = { -- statusline
+				hl = { fg = "fg", bg = "bg" },
+				status.component.mode({
+					mode_text = { padding = { left = 1, right = 1 } },
+				}), -- add the mode text
+				status.component.file_info({
+					filename = { fallback = "Empty" },
+					filetype = false,
+				}),
+				status.component.git_diff(),
+				status.component.diagnostics(),
+				status.component.fill(),
+				status.component.cmd_info(),
+				status.component.fill(),
+				status.component.git_branch(),
+				status.component.lsp(),
+				status.component.virtual_env(),
+				status.component.treesitter(),
+				status.component.nav(),
+				-- remove the 2nd mode indicator on the right
+			}
+		end,
 	},
 	-- {
 	-- 	"folke/trouble.nvim",
